21318aad5ea8ff936bee92f17275751843770528
Vit <vital.zhann@gmail.com>
2024-02-16 10:42:35 +0300

    Добавлено задание 01 из Продвинутое программирование

---
+++ 04_adv_progr_cpp/04_task_01_1/04_task_01_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <clocale>
+#include <locale.h>
+#include <windows.h>
+
+using namespace std; 
+
+//---------------------------------------------------------
+struct point {
+	int m_x, m_y;
+
+	point(int x, int y) {
+		m_x = x;
+		m_y = y;
+	}
+};
+
+//---------------------------------------------------------
+void print_point(point& point_object) {
+	cout << "x:" << point_object.m_x << ", y:"
+  << point_object.m_y << endl;
+}
+
+int main(int argc, char** argv)
+{
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+	
+	return 0;
+}

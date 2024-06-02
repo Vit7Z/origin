406f3934d514a6f9e5cd17775ccf68c3476033d7
Vit <vital.zhann@gmail.com>
2024-06-03 01:01:50 +0300

    Задание 03_task_063

--- 03_alg_and_struct_data/03_task_063/03_task_063.cpp
+++ 03_alg_and_struct_data/03_task_063/03_task_063.cpp
@@ -1,140 +1,100 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <vector>
 #include <clocale>
 #include <cmath>
 #include <locale.h>
 
-//-------------------------------------------------------------------
-void find_substring_light_rabin_karp(std::string& m_String, std::string& m_Key)
-{
-  signed long tmp { 0 };
-  signed long hashStr{ 0 };
-  signed long hashKey{ 0 };
+//--вычисление хэш для последовательности симоволов----------------------------
+signed long getHash(std::string& m_String, size_t m_Strlen) {
+  unsigned long long tmp{ 0 };
+  unsigned long long hashString{ 0 };
 
-  size_t m_Strlen = m_String.length() - 1;
-  size_t m_Keylen = m_Key.length() - 1;
-
-  /////// находим хэш ключа (подстроки)
-  for (int i = 0; i <= m_Keylen; i++) {
-    tmp = (signed long)m_Key[i] * (signed long)pow(53, i);
-    hashKey += tmp;
-
-    std::cout << "Хэш подстроки: " << hashKey << "\n";
+  for (int i = m_Strlen; i > 0; i--) {
+    tmp = ((int)m_String[m_Strlen - i] * (unsigned long long)pow(31, i - 1)) % 2147483647;
+    hashString += tmp;
   }
-
-  tmp = 0;
-
-  /////// находим хэш для строки, длиной ключа
-  for (int i = 0; i <= m_Keylen; i++) {
-    tmp = (signed long)m_String[i] * (signed long)pow(53, i);
-    hashStr += tmp;
-
-    std::cout << "Хэш строки длиной Key: " << hashStr << "\n";
-  }
-
-
-  for (int i = 1; i <= m_Strlen - m_Keylen; i++) {
-
-    //std::cout << i << "    hashSource " << hashSource << "\n";
-    //std::cout << "     " << strSource.substr(i, strKeylen + 1) << "\n";
-    //std::cout << "-" << GetHash(strSource.substr(i, 1), 1) << " для " << strSource.substr(i, 1);
-    //std::cout << "    +" << GetHash(strSource.substr(i + strKeylen, 1), 1) << " для " << strSource.substr(i + strKeylen, 1) << "\n";
-
-    //if (hashSource == hashKey) {
-    //  std::cout << "Хэш совпадения " << i << " " << hashSource << "\n";
-
-
-
-    //  //for (int j = i; j <= i + strKeylen; j++) {
-    //  //  //if () {
-
-    //  //  //}
-    //  //  std::cout << "       " << j << " " << strSource.substr(j, 1) << "\n";
-    //  //}
-
-    //}
-
-
-    //hashSource = hashSource - GetHash(strSource.substr(i, 1), 1) + GetHash(strSource.substr(i + strKeylen, 1), 1);
-  }
-
-
-
- 
+  return hashString;
 }
 
-//
+//-------------------------------------------------------------------
+void find_substring_light_rabin_karp(std::string& m_Key, std::string& m_Seq) {
+  unsigned long long hashKey{ 0 };
+  unsigned long long hashTmp{ 0 };
+
+  unsigned long long hashPlus{ 0 };
+  unsigned long long hashMinus{ 0 };
+
+  size_t m_Keylen = m_Key.length();
+  size_t m_Seqlen = m_Seq.length();
+
+  int s{ -1 };
+  std::vector<int> positions;
+
+  /////// находим хэш ключа (подстроки) ///////
+  hashKey = getHash(m_Key, m_Keylen);
+
+  for (int i = 0; i <= m_Seqlen - m_Keylen; i++) {
+
+    /////// находим хэш последовательности символов в m_Seq длиной ключа ///////
+    if (i == 0) {
+      hashTmp = getHash(m_Seq, m_Keylen);
+    }
+
+    /////// если хэши совпали ///////
+    if (hashKey == hashTmp) {
+      for (int j = i; j <= i + m_Keylen - 1; j++) {
+        if (m_Seq.substr(j, 1) == m_Key.substr(j - i, 1)) {
+          s = 0;
+        }
+      }
+
+      if (s == 0) {
+        positions.push_back(i);
+      }
+    }
+
+    hashMinus = (int)m_Seq[i] * (unsigned long long)pow(31, m_Keylen - 1);
+    hashTmp = (hashTmp - hashMinus) * 31;
+    hashPlus = (int)m_Seq[i + m_Keylen];
+    hashTmp = (hashTmp + hashPlus) % 2147483647;
+  }//for
+
+  /////// печать результата ///////
+  if (positions.empty()) {
+    positions.push_back(-1);
+    std::cout << "Подстрока " << m_Key << " не найдена." << "\n";
+  }
+  else {
+    std::cout << "Подстрока найдена (индекс): ";
+    for (auto k : positions) {
+      std::cout << " " << k << " ";
+    }
+    std::cout << "\n";
+  }
+}//find_substring_light_rabin_karp
+
 int main(int argc, char** argv)
 {
   setlocale(0, "Rus");
 
-  std::string strSource { "0" };
   std::string strKey { "0" };
-
-
-
-  //std::cout << "Введите строку, в которой будет осуществляться поиск: ";
-  //std::cin >> strSource;
-
-  //std::cout << "Введите подстроку, которую нужно найти: ";
-  //std::cin >> strKey;
-
-  //strSource = "hellohellomydearfriend";
-  //strKey = "hello";
-  //"rthellohellomydearfriend"
-  strSource = "rthellohellomydearfriend";
-  strKey = "hello";
-
-
-  find_substring_light_rabin_karp (strSource, strKey);
-
-
-
-
-
-
-
-
-
-  //std::cout << "strSource.length " << strSourcelen << "\n";
-  //std::cout << "strKey.length " << strKeylen << "\n";
-
-
-  //hashSource = GetHash(strSource, strKeylen);
-  //hashKey = GetHash(strKey, strKeylen);
-
-  //std::cout << "Хэш строки strSource: " << hashSource << "\n";
-  //std::cout << "Хэш строки strKey: " << hashKey << "\n";
-
-
-//  for (int i = 0; i <= strSourcelen - strKeylen; i++) {
-//
-//    std::cout << i << "    hashSource " << hashSource << "\n";
-//std::cout << "     " << strSource.substr(i, strKeylen + 1) << "\n";
-//std::cout << "-" << GetHash(strSource.substr(i, 1), 1) << " для " << strSource.substr(i, 1);
-//std::cout << "    +" << GetHash(strSource.substr(i + strKeylen, 1), 1) << " для " << strSource.substr(i + strKeylen, 1) << "\n";
-//
-//    if (hashSource == hashKey) {
-//      std::cout << "Хэш совпадения " << i << " " << hashSource << "\n";
-//
-//
-//
-//      for (int j = i; j <= i + strKeylen; j++) {
-//        //if () {
-//
-//        //}
-//        std::cout << "       " << j << " " << strSource.substr(j, 1) << "\n";
-//      }
-//
-//    }
-//
-//
-//    hashSource = hashSource - GetHash(strSource.substr(i, 1), 1) + GetHash(strSource.substr(i + strKeylen, 1), 1);
-//  }
+  std::string strSource { "0" };
   
+  std::cout << "Введите строку, в которой будет осуществляться поиск: ";
+  std::cin >> strSource;
+
+  while (strKey != "exit") {
+    std::cout << "Введите подстроку, которую нужно найти: ";
+    std::cin >> strKey;
+
+    if (strSource.size() < strKey.size()) {
+      std::cout << "Неправильный ввод: длина подстроки больше длины строки!" << "\n";
+    }
+    else {
+      find_substring_light_rabin_karp(strKey, strSource);
+    }
+  }
+
   return 0;
 }    
-
-
-

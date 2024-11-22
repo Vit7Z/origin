a1d7f5d26ac04dbfb86b587d533fd6a293c5ed17
Vit <vital.zhann@gmail.com>
2024-11-22 12:25:05 +0200

    Задание 06_task_0201

---
+++ 06_multithreading/06_task_0201/06_task_0201.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+void increase(unsigned int& maxNum, unsigned int& crntNum) {
+
+	static_cast <atomic<unsigned int>>(crntNum);
+
+	while (crntNum < maxNum) {
+		this_thread::sleep_for(chrono::seconds(1));
+		crntNum++;
+		cout << "Число клиентов (+): " << crntNum << std::endl;
+	}
+}
+
+void decrease(unsigned int& crntNum) {
+	static_cast <atomic<unsigned int>>(crntNum);
+
+	while (crntNum > 0) {
+		this_thread::sleep_for(chrono::seconds(2));
+		crntNum--;
+		cout << "Число клиентов (-): " << crntNum << std::endl;
+	}
+}
+
+
+
+int main() {
+	setlocale(0, "Rus");
+
+	unsigned int maxNumberClients {10};
+	unsigned int currentClients{ 1 };
+
+	cout << "Число клиентов (начало): " << currentClients << std::endl;
+
+	thread t_1(increase, ref(maxNumberClients), ref(currentClients));
+	t_1.detach();
+
+	thread t_2(decrease, ref(currentClients));
+	t_2.join();
+
+	return 0;
+}
